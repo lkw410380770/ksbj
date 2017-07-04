6a4579878abdc8934b325b9517de3366fc2c0f4e
lkw410380 <410380770@qq.com>
2017-07-04 19:00:37 +0800

    内页

--- js/index.js
+++ js/index.js
@@ -1,35 +1,7 @@
 $(function() {
 
-    // 返回顶部
-    var oGoTop = $('.goTop');
-    oGoTop.on('click',function(ev){
-          ev.preventDefault();
-          $("html,body").animate({scrollTop:0}, 500);
-    })
-
-    // 导航
-    var navLi = $('.n-list>ul>li>a');
-    navLi.mouseover(function() {
-        navLi.removeClass('active');
-        $(this).addClass('active');
-    });
-    var navBottom = $('.nav-bottom>ul>li>a');
-    navBottom.mouseover(function() {
-        navBottom.removeClass('active');
-        $(this).addClass('active');
-    });
-     var hideBtn = $('.hideBtn');
-    var oHideUl = $('.navListBox');
-    var mask = $('.mask');
-    hideBtn.on('click',function(){
-        mask.fadeIn();
-        // oHideUl.css('right',"0");
-       oHideUl.addClass('addTrans');
-    });
-    mask.on('click',function(){
-      mask.fadeOut();
-      oHideUl.removeClass('addTrans')
-    })
+   
+    
     /**
      * [mySwiper description] 班级介绍动画
      * @type {Swiper}
